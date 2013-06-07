d850635ca1fd47aa16c2572d2d2cc187381104f1
michaelporter <michaelporter.0988@gmail.com>
2013-06-07 14:51:59 -0400

    makes server a little more robust; adds dummy favicon.ico

--- server.js
+++ server.js
@@ -1,49 +1,62 @@
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
+var url = require('url');
 
 var requestListener = function(req, res) {
-  var fileName = "." + (req.url == '/' ? '/index.html' : req.url);
+  var defaultHeaders = {
+    "Content-Type": "text/plain"
+  };
 
-  var fileExt = getFileExtension(fileName);
-  var headers = getContentType(fileExt);
-  res.writeHead(200, headers);
+  var uri = url.parse(req.url).pathname
+  uri = uri == '/' ? 'index.html' : uri;
 
-  if (fileExt == '.js' || fileExt == '.html' || fileExt == '.css') {
-    var fileContent = fs.readFileSync(fileName);
-    res.write(fileContent);
-  }
+  var fileName = path.join(process.cwd(), uri);
+  var fileExt, fileContent;
 
-  res.end();
+  fs.exists(fileName, function(exist) {
+    if (!exist) {
+      console.log("error finding file");
+      res.writeHead(404, defaultHeaders);
+      res.end("error finding file");
+
+      return;
+    }
+
+    var fileContent = fs.readFile(fileName, 'utf8', function(err, data) {
+      var headers = defaultHeaders;
+
+      fileExt = getFileExtension(fileName);
+      headers["Content-Type"] = getContentType(fileExt);
+      headers["Content-Length"] = data.length;
+      res.writeHead(200, headers);
+
+      res.end(data);
+    });
+  });
 }
 
 var port = process.env.PORT || 8124;
 var server = http.createServer(requestListener).listen(port);
 
-console.log("server running");
+console.log("server running on port " + port);
 
 var getFileExtension = function(path) {
   var pathChunks = path.split(".");
   var extension = pathChunks[pathChunks.length-1];
 
-  return "." + extension;
+  return extension;
 }
 
 var getContentType = function(extension) {
-  var contentType;
-
-  switch(extension) {
-    case '.js':
-      contentType = 'text/javascript'; 
-      break;
-    case '.html':
-      contentType = 'text/html';
-      break;
-    case '.css':
-      contentType = 'text/css';
-      break;
-    default:
-      contentType = 'text/html';
+  var mimeTypes = {
+    'css' : 'text/css',
+    'html': 'text/html',
+    'ico' : 'image/ico',
+    'js'  : 'text/javascript'
   }
 
-  return { 'content-type': contentType };
+  var contentType = mimeTypes[extension] || 'text/html';
+  return contentType;
 }
+
